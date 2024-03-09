3edd6ad6c209a56d7b4a9f82b6f7ae4a26e4bae8
Noella Spitz <noella.spitz@gmail.com>
2024-03-09 17:42:02 +0000

    Improve functions in collateral contract

--- contracts/collateralise.sol
+++ contracts/collateralise.sol
@@ -17,13 +17,26 @@ contract Collateralise is Ownable {
 
     struct Ledger {
         address ledgerOwner;
-        Commit totalFunds;
+        uint256 totalBalance;
+        Commit totalRevenue;
+        Commit totalBalanceCommit;
     }
 
     error NotRegistered();
     error TransactionFailed();
+    error InsufficientFunds();
+
+    event FundsTransfered(address company, address to, uint256 x, uint256 y);
+    event Collateralise(address company, uint256 amount);
 
     constructor () Ownable(msg.sender) {}
+
+    modifier onlyRegistered() {
+        if (isCompanyRegistered[msg.sender] == false) {
+            revert NotRegistered();
+        }
+        _;
+    }
 
     function registerLedger() public {
         isCompanyRegistered[msg.sender] = true;
@@ -33,25 +46,42 @@ contract Collateralise is Ownable {
         );
     }
 
-    function collateraliseAssets(uint256 _bindingFactor) public payable {
-        if (isCompanyRegistered[msg.sender] == false) {
-            revert NotRegistered();
-        }
-
+    function collateraliseAssets(uint256 _bindingFactor) public payable onlyRegistered() {
         (uint256 x, uint256 y) = PedersenCommitment.commit(_bindingFactor, msg.value);
 
-        companyLedgers[msg.sender].Commit.x = x;
-        companyLedgers[msg.sender].Commit.y = y;
+        (uint256 xTot, uint256 yTot) = PedersenCommitment.eAdd(
+            x, y, companyLedgers[msg.sender].totalBalanceCommit.x, companyLedgers[msg.sender].totalBalanceCommit.y
+        );
+        companyLedgers[msg.sender].totalBalanceCommit.x = xTot;
+        companyLedgers[msg.sender].totalBalanceCommit.y = yTot;
     }
 
-    function allocateFunds(address _to, uint256 _amount, uint256 _bindingFactor) public {
-        if (isCompanyRegistered[msg.sender] == false) {
-            revert NotRegistered();
+    function allocateFunds(address _to, uint256 _amount, uint256 _bindingFactor) public onlyRegistered() {
+        if (_amount > companyLedgers[msg.sender].totalBalance) {
+            revert InsufficientFunds();
         }
 
-        if 
-        
+        (uint256 xAlloc, uint256 yAlloc) = PedersenCommitment.commit(_bindingFactor, _amount);
+        (uint256 xRemaining, uint256 yRemaining) = PedersenCommitment.eSub(
+            companyLedgers[msg.sender].totalRevenue.x, companyLedgers[msg.sender].totalRevenue.y, xAlloc, yAlloc
+        );
+
+        companyLedgers[msg.sender].totalRevenue = Commit(xRemaining, yRemaining);
+        allocations[_to] = PedersenCommitment.eAdd(allocations[_to].x, allocations[_to].y, xAlloc, yAlloc);
+
+        emit FundsTransfered(msg.sender, _to, xAlloc, yAlloc);
     }
+    
+    function moveFunds(address _from, uint256 _amount, uint256 _bindingFactor) public onlyRegistered() {
+
+    }
+
+    function verifyFunds(address _addr, uint256 _amount, uint256 _bindingFactor) public view returns (bool) {
+        return PedersenCommitment.verify(_bindingFactor, _amount, allocations[_addr].x, allocations[_addr].y);
+    }
+
+    function lock
+
 
     function withdraw(uint256 _amount) external onlyOwner {
         (bool sent,) = msg.sender.call{value: _amount}("");
