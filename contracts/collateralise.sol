6b0709a14986b12572db8cae5bd198624a2e298d
Noella Spitz <noella.spitz@gmail.com>
2024-03-09 15:42:13 +0000

    Store commits

--- contracts/collateralise.sol
+++ contracts/collateralise.sol
@@ -1,17 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {PedersenCommitment} from "./PedersenCommitment.sol";
 
 contract Collateralise is Ownable {
     // Mapping from company to verkle root
-    mapping(address => Ledger) public companyLedgers;
-    mapping(address => uint256) public allocations;
     mapping(address => bool) public isCompanyRegistered;
+    mapping(address => Ledger) public companyLedgers;
+    mapping(address => Commit) public allocations;
+
+    struct Commit {
+        uint256 x;
+        uint256 y;
+    }
 
     struct Ledger {
         address ledgerOwner;
-        uint256 rootHash;
+        Commit totalFunds;
     }
 
     error NotRegistered();
@@ -20,26 +26,30 @@ contract Collateralise is Ownable {
     constructor () Ownable(msg.sender) {}
 
     function registerLedger() public {
-        isCompanyRegistered[msg.sender] = true;// Commit?
+        isCompanyRegistered[msg.sender] = true;
         companyLedgers[msg.sender] = Ledger(
             msg.sender,
-            0
+            Commit(0, 0)
         );
     }
 
-    function collateraliseAssets(uint256 commitment) public payable {
+    function collateraliseAssets(uint256 _bindingFactor) public payable {
         if (isCompanyRegistered[msg.sender] == false) {
             revert NotRegistered();
         }
 
-        companyLedgers[msg.sender].rootHash = commitment;
+        (uint256 x, uint256 y) = PedersenCommitment.commit(_bindingFactor, msg.value);
+
+        companyLedgers[msg.sender].Commit.x = x;
+        companyLedgers[msg.sender].Commit.y = y;
     }
 
-    function allocateFunds(address _to, uint256 amount, uint256 commitment) public {
+    function allocateFunds(address _to, uint256 _amount, uint256 _bindingFactor) public {
         if (isCompanyRegistered[msg.sender] == false) {
             revert NotRegistered();
         }
 
+        if 
         
     }
 
