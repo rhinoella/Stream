ae0d429f8bd5a8356c470a3d82dcf55ba4ff0854
Noella Spitz <noella.spitz@gmail.com>
2024-03-09 00:56:14 +0000

    Update collateralise

--- contracts/collateralise.sol
+++ contracts/collateralise.sol
@@ -5,8 +5,14 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Collateralise is Ownable {
     // Mapping from company to verkle root
-    mapping(address => uint256) public companyLedgers;
+    mapping(address => Ledger) public companyLedgers;
+    mapping(address => uint256) public allocations;
     mapping(address => bool) public isCompanyRegistered;
+
+    struct Ledger {
+        address ledgerOwner;
+        uint256 rootHash;
+    }
 
     error NotRegistered();
     error TransactionFailed();
@@ -15,6 +21,10 @@ contract Collateralise is Ownable {
 
     function registerLedger() public {
         isCompanyRegistered[msg.sender] = true;// Commit?
+        companyLedgers[msg.sender] = Ledger(
+            msg.sender,
+            0
+        );
     }
 
     function collateraliseAssets(uint256 commitment) public payable {
@@ -22,7 +32,15 @@ contract Collateralise is Ownable {
             revert NotRegistered();
         }
 
-        companyLedgers[msg.sender] = commitment;
+        companyLedgers[msg.sender].rootHash = commitment;
+    }
+
+    function allocateFunds(address _to, uint256 amount, uint256 commitment) public {
+        if (isCompanyRegistered[msg.sender] == false) {
+            revert NotRegistered();
+        }
+
+        
     }
 
     function withdraw(uint256 _amount) external onlyOwner {
