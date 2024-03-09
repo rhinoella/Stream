20ccc4693c454f57f719d207d9ef7662cc048dd3
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-03-09 17:45:10 +0000

    ali: added witness func

--- src/zk.py
+++ src/zk.py
@@ -7,3 +7,18 @@ def commit(
     for idx, item in enumerate(poly_coeff):
         poly += item * s ** (len(poly_coeff) - idx - 1)
     return g ** poly
+
+
+def witness(
+        s: float, *, poly_coeff: list, g: float  
+) -> list[float]:
+    """
+    """
+    witness_array = []
+    for idx in range(len(poly_coeff)):
+        poly = 0.
+        for idx_0, item in enumerate(poly_coeff):
+            if idx != idx_0:
+                poly += item * s ** (len(poly_coeff) - idx_0 - 1)
+        witness_array.append(g ** poly)
+    return witness_array
