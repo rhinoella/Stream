321ac19faab06eefc7bc2fdcb2bbd1f7d59e6da5
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-10 00:36:20 +0000

    ali: added checks.is_prime()

---
+++ src/utils/checks.py
@@ -0,0 +1,12 @@
+import math
+
+
+def is_prime(n: int) -> bool:
+    """
+    """
+    if n % 2 == 0 and n > 2:
+        return False
+    return all(
+        n % i
+        for i in range(3, int(math.sqrt(n)) + 1, 2)
+    )
