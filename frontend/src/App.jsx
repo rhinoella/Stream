40b487262b69fa532e3c91871491e501c4a6541a
Noella Spitz <noella.spitz@gmail.com>
2024-03-10 05:37:25 +0000

    Connect to modal, fix eth value calculations, get eth balance from user, add logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,46 @@
 // App.jsx
 import React, { useState } from 'react';
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers/react'
 import CompanyPage from './CompanyPage';
 import BankPage from './BankPage';
 import AuditPage from './AuditPage';
 import './App.css';
+
+// 1. Get projectId
+const projectId = 'dac81c2220eeaf06d3861337202036a7'
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+const testnet = {
+  chainId: 11155111,
+  name: 'Sepolia',
+  currency: 'SepoliaETH',
+  explorerUrl: 'https://sepolia.etherscan.io',
+  rpcUrl: 'https://sepolia.infura.io/v3/'
+}
+
+// 3. Create modal
+const metadata = {
+  name: 'Stream',
+  description: 'Streamline your cashflows with Stream',
+  url: 'https://mywebsite.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.mywebsite.com/']
+}
+
+createWeb3Modal({
+  ethersConfig: defaultConfig({ metadata }),
+  chains: [testnet],
+  projectId,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
 
 const App = () => {
   const [activePage, setActivePage] = useState(<CompanyPage/>);
@@ -16,8 +53,11 @@ const App = () => {
         <button onClick={() => setActivePage(<BankPage/>)}>Bank</button>
         <button onClick={() => setActivePage(<AuditPage/>)}>Audit</button>
         </div>
+        <div className="spacer"></div>
+        <div id="logo-group"><img src={"/streamIcon.png"} height={50} width={50}></img><p id="logo">stream</p></div>
+        <div className="spacer"></div>
         <div className="link-wallet-btn-container">
-          <button className="link-wallet-btn">Link a Wallet</button>
+          <w3m-button />
        </div>
       </header>
       {activePage}
--- frontend/src/CompanyPage.jsx
+++ frontend/src/CompanyPage.jsx
@@ -1,48 +1,102 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers/react'
+import { BrowserProvider, Contract, formatEther, formatUnits, parseEther } from 'ethers'
 import BalanceDisplay from './BalanceDisplay';
 import SalariesAllocation from './SalariesAllocation';
 
+const FLARE_RPC = "https://flare-api.flare.network/ext/bc/C/rpc";
+
+const collateraliseContract = '0x617f3112bf5397D0467D315cC709EF968D9ba546'
+const collateraliseABI = [
+  'modifier onlyRegistered()',
+  'function registerLedger() public',
+  'function collateraliseAssets(uint256 _bindingFactor) public payable onlyRegistered()',
+  'function allocateFunds(address _to, uint256 _amount, uint256 _bindingFactor) public onlyRegistered()'
+]
+
 const CompanyPage = () => {
-  const [totalBalance, setTotalBalance] = useState(10000);
-  const [currentBalance, setCurrentBalance] = useState(10000);
+  const [totalUSDBalance, setTotalUSDBalance] = useState(0);
+  const [totalETHBalance, setTotalETHBalance] = useState(0);
+  const [currentBalance, setCurrentBalance] = useState(0);
+  const [totalDue, setTotalDue] = useState(0);
   const [totalSpent, setTotalSpent] = useState(0); // Track the total spent here
   const [employees, setEmployees] = useState([
     { id: 1, name: 'Employee1', salary: 0 },
     { id: 2, name: 'Employee2', salary: 0 },
     { id: 3, name: 'Employee3', salary: 0 },
   ]);
+  const [ethPrice, setEthPrice] = useState(0);
+  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
+
+  useEffect(() => {
+    const fetchEthPrice = async () => {
+      const ethers = await import("ethers");
+      const flare = await import("@flarenetwork/flare-periphery-contract-artifacts");
+
+      const provider = new ethers.JsonRpcProvider(FLARE_RPC);
+  
+      const flareContractRegistry = new ethers.Contract(
+          "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019",
+          flare.nameToAbi("FlareContractRegistry", "flare").data,
+          provider);
+  
+      const ftsoRegistryAddr = await
+          flareContractRegistry.getContractAddressByName("FtsoRegistry");
+      const ftsoRegistry = new ethers.Contract(
+          ftsoRegistryAddr,
+          flare.nameToAbi("FtsoRegistry", "flare").data,
+          provider);
+  
+      const [_price, _timestamp, _decimals] =
+          await ftsoRegistry["getCurrentPriceWithDecimals(string)"]("ETH");
+
+      setEthPrice(Number(await _price));
+    }
+
+    fetchEthPrice();
+
+  }, [walletProvider, isConnected]);
+
+  useEffect(() => {
+    const getUserBalance = async () => {
+      if (ethPrice && isConnected && walletProvider) {
+        const ethersProvider = new BrowserProvider(walletProvider);
+        const balance = await ethersProvider.getBalance(address);
+        console.log(formatUnits(balance))
+        console.log(ethPrice/100000)
+        setTotalETHBalance(formatUnits(balance));
+        setTotalUSDBalance((ethPrice/100000) * formatUnits(balance));
+      }
+    };
+
+    if (ethPrice !== 0) {
+      getUserBalance();
+    }
+  }, [ethPrice]);
 
   const handleAllocateSalary = (updatedEmployees) => {
     setEmployees(updatedEmployees);
     const totalSalaries = updatedEmployees.reduce((total, employee) => total + employee.salary, 0);
     setTotalSpent(totalSalaries);
-    setCurrentBalance(totalBalance - totalSalaries);
+    setTotalDue(parseFloat(totalSpent / ethPrice *100000).toFixed(4));
+    setCurrentBalance(totalUSDBalance - totalSalaries);
   };
 
   const paySalaries = () => {
-    const totalSalaries = employees.reduce((total, employee) => total + (employee.salary || 0), 0);
-    if (totalSalaries <= currentBalance) {
-      setTotalBalance(prev => prev - totalSalaries); // Deduct salaries from the total balance
-      setTotalSpent(prev => prev + totalSalaries); // Add salaries to the total spent
-      setCurrentBalance(prev => prev - totalSalaries); // Update current balance
-      const resetEmployees = employees.map(employee => ({ ...employee, salary: 0 }));
-      setEmployees(resetEmployees);
-      alert('Salaries paid!');
-    } else {
-      alert('Not enough balance to pay salaries');
-    }
+
   };
 
   return (
     <>
-      <BalanceDisplay totalBalance={totalBalance} currentBalance={currentBalance} />
+      <BalanceDisplay totalUSDBalance={totalUSDBalance} totalETHBalance={totalETHBalance} currentBalance={currentBalance} ethPrice={ethPrice} />
       <SalariesAllocation
         employees={employees}
-        onAllocateSalary={handleAllocateSalary}
+        onSalaryChange={handleAllocateSalary}
       />
       <div className="spending-summary">
         <div className="spent">
-          Spent: {totalSpent.toFixed(2)} USD
+          Due: {totalSpent.toFixed(2)} USD / {totalDue} ETH
           {/* Calculate and display the equivalent in Ether */}
         </div>
         <button onClick={paySalaries} className="pay-salaries-btn">
