3ab9df01ec4825d9c0d033256006c9250f69427d
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-10 16:04:40 +0000

    zk.js amended

--- frontend/src/zk.js
+++ frontend/src/zk.js
@@ -1,58 +1,67 @@
 // Generator of cyclic group G
 const g = BigInt(3);
 // Secret
-const secret = BigInt(7);
+const secret = BigInt(3);
+// polynomial coefficients
+const polyCoeff = [1, 2, 3, 4, 5, 6];
 
-const mod = BigInt(1000000000000000000000000000000000) //BigInt(193939);
+//const mod = BigInt(1000000000000000000000000000000000) //BigInt(193939);
 
 // Ys is polynomial evaluated at secret point
-const commitPoly = (polyCoeff) => {
+function commitPoly(polyCoeff) {
     let poly = 0n;
     const userCommits = [];
     const elementPoly = [];
-    const witnessArray = [];
-    const polyLength = polyCoeff.length;
 
-    for (let idx = 0; idx < polyLength; idx++) {
-        const currentPoly = (polyCoeff[idx] * ((secret ** BigInt(idx)) % mod)) % mod;
+    for (let idx = 0; idx < polyCoeff.length; idx++) {
+        const currentPoly = polyCoeff[idx] * (secret ** (polyCoeff.length - BigInt(idx) - 1));
         elementPoly.push(currentPoly);
-        poly = (poly + currentPoly) % mod;
-        console.log(poly)
-        userCommits.push((g ** currentPoly) % mod);
+        poly += currentPoly;
+        console.log(poly);
+        userCommits.push((g ** currentPoly));
     }
 
-    elementPoly.forEach(element => {
-        console.log(g ** (poly - element))
+    //elementPoly.forEach(element => {
+    //    console.log(g ** (poly - element))
+    //    witnessArray.push((g ** ((poly - element) % mod)) % mod);
+    //});
 
-        witnessArray.push((g ** ((poly - element) % mod)) % mod);
-    });
+    return [(g ** poly), userCommits];
+};
 
-    return [(g ** poly) % mod, witnessArray, userCommits];
-}
 
-const generateWitnesses = (polyCoeff, s, g) => {
-    let witnessArray = [];
-    const polyLength = polyCoeff.length;
+function generateWitnesses(polyCoeff) {
+    const witnessArray = [];
 
-    for (let idx = 0; idx < polyLength; idx++) {
+    for (let idx = 0; idx < polyCoeff.length; idx++) {
         let poly = 0n;
-        for (let idx_0 = 0; idx_0 < polyLength; idx_0++) {
+        for (let idx_0 = 0; idx_0 < polyCoeff.length; idx_0++) {
             if (idx !== idx_0) {
-                const currentPoly = (polyCoeff[idx_0] * (s ** BigInt(idx_0)) % mod) % mod;
-                poly += currentPoly % mod;
-            }
-        }
-        witnessArray.push((g ** poly) % mod);
+                const currentPoly = polyCoeff[idx_0] * (secret ** (polyCoeff.length - BigInt(idx_0) - 1));
+                poly += currentPoly;
+            };
+        };
+        witnessArray.push((g ** poly));
     }
 
     return witnessArray;
-}
-
-const verify = (userCommit, witness, aggCommit) => {
-    console.log(aggCommit)
-    console.log(witness)
-    console.log((witness * userCommit) % mod)
-    return ((witness * userCommit) % mod) == (aggCommit)
 };
 
-export {g, secret, commitPoly, generateWitnesses, verify};
+
+function verify(userCommit, witness, aggCommit) {
+    if (aggCommit / witness == userCommit) {
+        return 1;
+    } else {
+        return 0;
+    };
+};
+
+
+const [aggCommit, userCommits] = commitPoly(polyCoeff);
+const witnesses = generateWitnesses(polyCoeff);
+const verifications = [];
+for (let idx = 0; idx < witnesses.length; idx++) {
+    verifications.push(verify(userCommits[idx], witnesses[idx], aggCommit))
+}
+
+export {g, secret, aggCommit, userCommits, witnesses, verifications};
