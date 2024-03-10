074515ba31e80394c39f8da79d06b1d0164c77d8
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-10 00:57:23 +0000

    ali: modifications to checks.py for prime n choice

--- src/pipeline/elliptic.py
+++ src/pipeline/elliptic.py
@@ -1,19 +1,20 @@
+import random
 from typing import Tuple
 
-import numpy as np
-from numpy.typing import NDArray
+from utils import checks
 
 
-def construct_func(
-        coeff: list[float], *, x_domain: Tuple[float, float], dx: float=0.01
-) -> NDArray:
+def extract_primes(lims: Tuple[int, int]) -> list[int]:
     """
     """
-    return np.asarray(
-        [
-            coeff[0] * n ** 3. +
-            coeff[1] * n +
-            coeff[2]
-        ]
-        for n in np.arange(x_domain[0], x_domain[1], dx)
-    )
+    return [
+        item 
+        for item in range(lims[0], lims[1])
+        if checks.is_prime(item)
+    ]
+
+
+def select_prime(input: list[int]) -> int:
+    """
+    """
+    return random.choice(input)
